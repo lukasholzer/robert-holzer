9f58d34c86441d83c7d578529cd72a7fd6c8585c
Lukas Holzer <office@lukasholzer.com>
2017-04-21 18:33:55 +0200

    add soil for clean wordpress

--- theme/functions.php
+++ theme/functions.php
@@ -8,7 +8,16 @@ add_theme_support( 'post-formats' );
 add_theme_support( 'post-thumbnails' );
 add_theme_support( 'menus' );
 
-// add_action('wp_enqueue_scripts', 'add_theme_scripts');
+add_theme_support('soil-clean-up');
+add_theme_support('soil-disable-asset-versioning');
+add_theme_support('soil-disable-trackbacks');
+add_theme_support('soil-google-analytics', 'UA-XXXXX-Y');
+add_theme_support('soil-jquery-cdn');
+add_theme_support('soil-js-to-footer');
+add_theme_support('soil-nav-walker');
+add_theme_support('soil-nice-search');
+add_theme_support('soil-relative-urls');
+
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts', 0);
 
 
