ed0fbffe8468a186c0d963db823be81b30de875d
Lukas Holzer <office@lukasholzer.com>
2017-05-20 22:04:05 +0200

    add Music player

--- theme/src/app/app.component.ts
+++ theme/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component, ElementRef } from 'mojiito-core';
 import { HeaderComponent } from './overlay.component';
 import { RepertoireComponent } from './repertoire.component';
+import { MusicplayerComponent } from './musicplayer.component';
 
 @Component({
     selector: 'body',
-    components: [HeaderComponent, RepertoireComponent]
+    components: [
+      HeaderComponent,
+      RepertoireComponent,
+      MusicplayerComponent
+    ]
 })
 export class AppComponent {
 
