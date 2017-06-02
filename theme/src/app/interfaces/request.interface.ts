6ec3120aceef312018b3f4b79f6fd591bf0cab99
Lukas Holzer <office@lukasholzer.com>
2017-06-02 18:39:36 +0200

    add IRequestOptions interface

--- theme/src/app/interfaces/request.interface.ts
+++ theme/src/app/interfaces/request.interface.ts
@@ -1,3 +1,18 @@
 export interface IRequest {
 
 }
+
+export interface IRequestOptions {
+  method?: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'HEAD' | 'OPTIONS',
+  headers?: any,
+  body?: any,
+  mode?: string, // not yet supported
+  credentials?: {
+    username: string;
+    password?: string;
+  },
+  cache?: 'default' | 'no-store' | 'reload' | 'no-cache' | 'force-cache' | 'only-if-cached', // not yet supported
+  redirect?: 'follow' | 'error' | 'manual', // not yet supported
+  referrer?: string, // not yet supported
+  integrity?: string // not yet supported
+}
