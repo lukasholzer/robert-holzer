58d110959ba680f4f8569e044731df64dc412678
Lukas Holzer <office@lukasholzer.com>
2017-06-02 19:16:14 +0200

    Request service

--- theme/src/app/services/request.service.ts
+++ theme/src/app/services/request.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Injector, ElementRef } from 'mojiito-core';
+import { IRequestOptions } from '../interfaces/request.interface';
 
 @Injectable()
 export class RequestService {
@@ -7,44 +8,16 @@ export class RequestService {
   }
 
 
-  public fetch(url: string): Promise<string> {
+  public fetch(url: string, options: IRequestOptions = {}): Promise<string> {
 
     return new Promise((resolve: (result: any) => void, reject: (reason: Error) => void) => {
-      let xhr = new XMLHttpRequest();
-    });
-
-  }
-
-/*
-  public fetch(url: string, options?: IRequestOptions): Promise<string> {
-    return new Promise((resolve: (result: any) => void, reject: (reason: Error) => void) => {
-      if (!options) {
-        options = {};
-      }
-
       let xhr = new XMLHttpRequest();
       let headers = options.headers;
 
       // Check if method is GET and body ias an object
       // If it is so, parse body object an append all properties as GET parameters to the url
       if (options.method === 'GET' && options.body) {
-        if (typeof options.body === 'object') {
-          let separator = '';
-          let uri = '';
-          for (var key in options.body) {
-            if (options.body.hasOwnProperty(key)) {
-              uri += `${separator}${key}=${options.body[key]}`
-              separator = '&';
-            }
-          }
-
-          // append the parameters to the url
-          url = url + (url.indexOf('?') === - 1 ? '?' : '&') + uri;
-        } else {
-          // Body is not an object => not allowed
-          reject(new TypeError('Non object like body not allowed for GET requests. The body has to be an object so the properties will be appended as a GET parameter to the url.'));
-          return;
-        }
+        url = this.generateGetParams(url, options);
       } else if (typeof options.body !== 'string') {
         // if body is not a string, parse it to a json string
         options.body = JSON.stringify(options.body);
@@ -56,6 +29,7 @@ export class RequestService {
         return;
       }
 
+
       // Add onload event listener
       xhr.onload = (event: Event) => {
         if (xhr.status >= 400 || xhr.status === 0) {
@@ -63,22 +37,22 @@ export class RequestService {
         } else {
           resolve('response' in xhr ? xhr.response : xhr.responseText);
         }
-      }
+      };
 
       // Add onerror event listener
       xhr.onerror = (event: ErrorEvent) => {
         debugger;
         reject(event.error);
-      }
+      };
 
       xhr.onabort = (event: ErrorEvent) => {
         reject(new TypeError('Network request aborted'));
-      }
+      };
 
       // Add timeout event listener
       xhr.ontimeout = (event: Event) => {
         reject(new TypeError('Network request timed out'));
-      }
+      };
 
       if (options.credentials && options.credentials.username) {
         xhr.open(options.method ? options.method : 'GET', url, true, options.credentials.username, options.credentials.password);
@@ -95,10 +69,44 @@ export class RequestService {
       }
 
       xhr.send(typeof options.body === 'undefined' ? null : options.body);
+
     });
+
   }
 
-  */
+  public generateGetParams(url: string, options: IRequestOptions) {
+    let separator = '';
+    let uri = '';
+
+    if (typeof options.body === 'object') {
+      for (let key in options.body) {
+        if (options.body.hasOwnProperty(key)) {
+          uri += `${separator}${key}=${options.body[key]}`
+          separator = '&';
+        }
+      }
+
+      // append the parameters to the url
+      return url + (url.indexOf('?') === - 1 ? '?' : '&') + uri;
+    } else {
+      // Body is not an object => not allowed
+      Promise.reject(new TypeError('Non object like body not allowed for GET requests. The body has to be an object so the properties will be appended as a GET parameter to the url.'));
+      return;
+    }
+  }
+
+  public fetchJSON(url: string, options: IRequestOptions = {}): Promise<Object | Array<any>> {
+
+    if (!options.headers) {
+      options.headers = {};
+    }
+    if (!options.headers['Content-Type']) {
+      options.headers['Content-Type'] = 'application/json';
+    }
+    return this.fetch(url, options).then(result => {
+      return JSON.parse(result);
+    });
+  }
 
 
 }
