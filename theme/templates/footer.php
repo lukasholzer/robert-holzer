e2d97736523a83c769197f70e44083ce0efda7e7
Lukas Holzer <office@lukasholzer.com>
2017-06-02 18:33:32 +0200

    add icons for music-player

--- theme/templates/footer.php
+++ theme/templates/footer.php
@@ -4,6 +4,8 @@
   $context = Timber::get_context();
   // $context['footer'] = Timber::get_posts($args);
 
-  $footer = array( 'components/footer.twig' );
+  $footer = array( 'components/footer.twig');
+  $icons = array( 'components/icons.twig' );
 
+  Timber::render( $icons, $context );
   Timber::render( $footer, $context );
