57f7c9eb0122d7730ce6da5c5078a4b54ca70ffd
Lukas Holzer <office@lukasholzer.com>
2017-06-05 00:19:57 +0200

    interface update

--- theme/src/app/interfaces/request.interface.ts
+++ theme/src/app/interfaces/request.interface.ts
@@ -16,18 +16,3 @@ export interface IRequestOptions {
 export interface IRequestResponse {
   response: Object;
 }
-
-export interface IRequestOptions {
-  method?: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'HEAD' | 'OPTIONS',
-  headers?: any,
-  body?: any,
-  mode?: string, // not yet supported
-  credentials?: {
-    username: string;
-    password?: string;
-  },
-  cache?: 'default' | 'no-store' | 'reload' | 'no-cache' | 'force-cache' | 'only-if-cached', // not yet supported
-  redirect?: 'follow' | 'error' | 'manual', // not yet supported
-  referrer?: string, // not yet supported
-  integrity?: string // not yet supported
-}
