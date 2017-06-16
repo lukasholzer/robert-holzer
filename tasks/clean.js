9da3545faf4ac60b3cffafd77306845cd1f44b2d
Lukas Holzer <office@lukasholzer.com>
2017-06-16 11:20:31 +0200

    remove tasks

--- tasks/clean.js
+++
@@ -1,9 +0,0 @@
-import del from 'promised-del';
-import path from 'path';
-import { base } from './config';
-
-const dist = path.resolve(__dirname, '..', 'theme', 'dist');
-
-export default function clean() {
-  return del([dist]);
-}
