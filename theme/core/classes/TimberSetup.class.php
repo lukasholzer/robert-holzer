f99d9ec35c0f2de5421fc3096ae47de15742a9c6
Lukas Holzer <office@lukasholzer.com>
2017-06-22 22:42:20 +0200

    split to wordpress and timber setup

--- theme/core/classes/TimberSetup.class.php
+++ theme/core/classes/TimberSetup.class.php
@@ -8,110 +8,11 @@ class Setup extends TimberSite {
 
 	function __construct() {
 
-    $this->theme_path = get_template_directory() . '/dist/';
-    $this->theme_uri = get_template_directory_uri() . '/dist/';
-
-    $this->version = wp_get_theme()->get( 'Version' );
-    $this->assets = (WP_ENV == 'development')? 'http://localhost:4000/': $this->theme_uri;
-
-		add_theme_support( 'post-formats' );
-    add_theme_support( 'post-thumbnails' );
-    add_theme_support( 'menus' );
-
-    add_action( 'after_setup_theme', array($this, 'robertholzer_custom_header_setup'));
-
-    // clean up wordpress
-    add_theme_support('soil-clean-up');
-    add_theme_support('soil-disable-asset-versioning');
-    add_theme_support('soil-disable-trackbacks');
-    add_theme_support('soil-google-analytics', 'UA-XXXXX-Y');
-    add_theme_support('soil-jquery-cdn');
-    add_theme_support('soil-js-to-footer');
-    add_theme_support('soil-nav-walker');
-    add_theme_support('soil-nice-search');
-    add_theme_support('soil-relative-urls');
-
 		add_filter('timber_context', array($this, 'add_to_context'));
 		add_filter('get_twig', array($this, 'add_to_twig'));
 
-    add_filter('acf/settings/save_json', array($this, 'save_acf_fields'));
-
-		add_action('init', array($this, 'register_post_types'));
-		add_action('init', array($this, 'register_taxonomies'));
-		add_action('init', array($this, 'register_menus'));
-
-    add_action('wp_enqueue_scripts',  array($this, 'add_theme_scripts'));
-
-    // $options = new OptionsPage('Allgemeine Theme Konfiguration', 'Theme Konfiguration', 'theme-options');
-    // $function = $options->addOptionsPage();
-    // var_dump($options);
-
 		parent::__construct();
 	}
-
-  function register_menus() {
-    register_nav_menus(
-      array(
-        'page-menu'     => __( 'Seiten Men&uuml;', 'robertholzer-theme' ),
-        'category-menu'    => __( 'Kategorie Men&uuml;', 'robertholzer-theme' ),
-        'gallery-menu'    => __( 'Gallerie Men&uuml;', 'robertholzer-theme' ),
-        'teaching-menu'    => __( 'Uni Men&uuml;', 'robertholzer-theme' )
-      )
-    );
-  }
-
-  function register_post_types() {
-    $repertoire = new CustomPostTypes('repertoire', 'Repertoire', 'Repertoire', 'dashicons-playlist-audio');
-    $press = new CustomPostTypes('press', 'Presse Artikel', 'Presse Artikeln', 'dashicons-testimonial');
-    $gallery = new CustomPostTypes('gallery', 'Gallerie', 'Gallerien', 'dashicons-format-gallery');
-    $music = new CustomPostTypes('music', 'Album', 'Alben', 'dashicons-format-audio');
-    $track = new CustomPostTypes('song', 'Track', 'Tracks', 'dashicons-controls-volumeon');
-  }
-
-  function register_taxonomies() {
-    // $slug, $name, $namePlural, $hirarchy, $registerFor = array('post'), $i18nDomain = 'robertholzer-theme'
-    $composer = new CustomTaxonomies('composer', 'Komponist', 'Komponisten', false, array('repertoire', 'music', 'song'));
-    $songtype = new CustomTaxonomies('songtype', 'Filter', 'Filter', false, array('post', 'repertoire', 'music', 'song', 'gallery', 'press'));
-    $gallery = new CustomTaxonomies('gallery', 'Kategorie', 'Kategorien', false, array('gallery'));
-  }
-
-  function save_acf_fields( $path ) {
-    // update path
-    $path = get_template_directory() . '/core/acf';
-
-    return $path;
-  }
-
-  function add_theme_scripts() {
-
-
-    wp_enqueue_script( 'polyfills', $this->assets('polyfills.bundle.js'), array(), $this->version, true );
-    wp_enqueue_script( 'vendor', $this->assets('vendor.bundle.js'), array('polyfills'), $this->version, true );
-    wp_enqueue_script( 'app', $this->assets('app.bundle.js'), array('vendor'), $this->version, true );
-
-    if(WP_ENV === 'development') {
-      wp_enqueue_script( 'inline', $this->assets('inline.bundle.js'), array('app'), $this->version, true );
-      wp_enqueue_script( 'main', $this->assets('main.bundle.js'), array('inline'), $this->version, true );
-    } else {
-      wp_enqueue_style( 'inline', $this->assets('inline.css'), array(), $this->version);
-      wp_enqueue_style( 'main', $this->assets('main.bundle.css'), array(), $this->version);
-    }
-  }
-
-  function file_content($file) {
-    return @file_get_contents($this->theme_path . $file);
-  }
-
-  function robertholzer_custom_header_setup() {
-    $args = array(
-      'default-image'         => $this->assets('default_header.jpg'), // Default Header Image to display
-      'width'                 => 1442, // Header image width (in pixels)
-      'flex-height'           => true,
-      'height'                => 1026, // Header image height (in pixels)
-    );
-
-    add_theme_support( 'custom-header', $args );
-  }
 
 	function add_to_context( $context ) {
 		$context['version'] = $this->version;
--- theme/functions.php
+++ theme/functions.php
@@ -9,6 +9,7 @@ if (class_exists('Timber')) {
   require_once __DIR__ . '/core/classes/CustomPostTypes.class.php';
   require_once __DIR__ . '/core/classes/CustomTaxonomies.class.php';
   require_once __DIR__ . '/core/options.php';
+  require_once __DIR__ . '/core/classes/WordpressSetup.class.php';
   require_once __DIR__ . '/core/classes/TimberSetup.class.php';
 
   define('WP_ENV', 'development');
