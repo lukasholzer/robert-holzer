4f36b1ce8e4ab15957753fe4b340005954a76501
Lukas Holzer <office@lukasholzer.com>
2017-06-27 17:08:28 +0200

    updates to structure of views

--- theme/templates/biography.php
+++ theme/templates/biography.php
@@ -2,3 +2,17 @@
   /*
    * Template Name: Biographie
    */
+
+
+
+  $context = Timber::get_context();
+  $context['post'] = new Timber\Post();
+
+  $context['component'] = array(
+    'title' => get_the_title()
+  );
+
+  $template = array( 'components/biography.twig' );
+
+  Timber::render( $template, $context );
+?>
