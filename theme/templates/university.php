aabd9a2dbe48b4237817061bd80b80cef6cc3fc0
Lukas Holzer <office@lukasholzer.com>
2017-07-04 11:50:33 +0200

    add uni

--- theme/templates/university.php
+++ theme/templates/university.php
@@ -2,3 +2,19 @@
   /*
   Template Name: Universität
   */
+
+  $args = array(
+    'post_type' => 'post',
+    'per_page' => '-1'
+  );
+
+  $context = Timber::get_context();
+  $context['posts'] = Timber::get_posts($args);
+  $context['component'] = 'univercity';
+
+  $uni = array( 'components/univercity.twig' );
+
+  Timber::render( $uni, $context );
+
+  ?>
+</section>
