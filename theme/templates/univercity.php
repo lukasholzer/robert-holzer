45df1995ec24a0f6a85a19d1aa471c6456235ce9
Lukas Holzer <office@lukasholzer.com>
2017-07-08 12:07:45 +0200

    render university

--- theme/templates/univercity.php
+++ theme/templates/univercity.php
@@ -1,25 +1,23 @@
 <?php
   /*
-  Template Name: Universität
-  */
+   * Template Name: Universität
+   */
 
-  // $args = array(
-  //   'post_type' => 'post',
-  //   'per_page' => '-1'
-  // );
+  $args = array(
+    'post_type' => 'post',
+    'per_page' => '-1'
+  );
 
-  // $context = Timber::get_context();
-  // $context['posts'] = Timber::get_posts($args);
+  $context = Timber::get_context();
+  $context['posts'] = Timber::get_posts($args);
 
-  // $context['component'] = array(
-  //   'name' => 'univercity',
-  //   'title' => get_the_title()
-  // );
+  $context['component'] = array(
+    'name' => 'univercity',
+    'title' => get_the_title()
+  );
 
-  // $uni = array( 'components/univercity.twig' );
+  $uni = array( 'components/univercity.twig' );
 
-  // Timber::render( $uni, $context );
+  Timber::render( $uni, $context );
 
   ?>
-
-  aöskjdfölkajsdölfjk
