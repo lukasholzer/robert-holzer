6f4c17db60f217b971badb7cd2732bc479232153
Lukas Holzer <office@lukasholzer.com>
2017-07-08 12:04:41 +0200

    remove wrong tag

--- theme/templates/univercity.php
+++ theme/templates/univercity.php
@@ -3,22 +3,23 @@
   Template Name: Universität
   */
 
-  $args = array(
-    'post_type' => 'post',
-    'per_page' => '-1'
-  );
+  // $args = array(
+  //   'post_type' => 'post',
+  //   'per_page' => '-1'
+  // );
 
-  $context = Timber::get_context();
-  $context['posts'] = Timber::get_posts($args);
+  // $context = Timber::get_context();
+  // $context['posts'] = Timber::get_posts($args);
 
-  $context['component'] = array(
-    'name' => 'univercity',
-    'title' => get_the_title()
-  );
+  // $context['component'] = array(
+  //   'name' => 'univercity',
+  //   'title' => get_the_title()
+  // );
 
-  $uni = array( 'components/univercity.twig' );
+  // $uni = array( 'components/univercity.twig' );
 
-  Timber::render( $uni, $context );
+  // Timber::render( $uni, $context );
 
   ?>
-</section>
+
+  aöskjdfölkajsdölfjk
